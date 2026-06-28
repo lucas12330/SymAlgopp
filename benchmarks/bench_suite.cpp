0fd62d89c8324677e5a7be5b02e52458b79d6172
Lucas Bezanilla <ligueketoniejudo@gmail.com>
2026-06-28 22:06:22 +0200

    chore: ajout des benchmarks pour l'integration definie, la limite et les series (DL)

--- benchmarks/bench_suite.cpp
+++ benchmarks/bench_suite.cpp
@@ -82,4 +82,82 @@ static void BM_SymAlgo_EqDiff_MatriceCompagnon(benchmark::State& state) {
 // Tester pour les ordres 1, 3, 5, 7, 9, 11, 13, 15
 BENCHMARK(BM_SymAlgo_EqDiff_MatriceCompagnon)->DenseRange(1, 15, 2);
 
+// ---------------------------------------------------------
+// 4. Benchmark: Limite en x = 0 (sin(x)/x)
+// ---------------------------------------------------------
+static void BM_SymAlgo_Limite(benchmark::State& state) {
+    ExprPtr expr = ast_sin(var("x")) / var("x");
+    EquationClassique eq(expr);
+
+    for (auto _ : state) {
+        EquationClassique* lim = eq.limite(0.0);
+        benchmark::DoNotOptimize(lim);
+        delete lim;
+    }
+}
+BENCHMARK(BM_SymAlgo_Limite);
+
+static void BM_GiNaC_Limite(benchmark::State& state) {
+    GiNaC::symbol x("x");
+    GiNaC::ex eq = GiNaC::sin(x) / x;
+
+    for (auto _ : state) {
+        GiNaC::ex s = eq.series(x==0, 1);
+        benchmark::DoNotOptimize(s);
+    }
+}
+BENCHMARK(BM_GiNaC_Limite);
+
+// ---------------------------------------------------------
+// 5. Benchmark: Développement Limité (Ordre 5)
+// ---------------------------------------------------------
+static void BM_SymAlgo_DL(benchmark::State& state) {
+    ExprPtr expr = ast_exp(var("x")) * ast_sin(var("x"));
+    EquationClassique eq(expr);
+
+    for (auto _ : state) {
+        EquationClassique* dl = eq.DL(0.0, 5);
+        benchmark::DoNotOptimize(dl);
+        delete dl;
+    }
+}
+BENCHMARK(BM_SymAlgo_DL);
+
+static void BM_GiNaC_DL(benchmark::State& state) {
+    GiNaC::symbol x("x");
+    GiNaC::ex eq = GiNaC::exp(x) * GiNaC::sin(x);
+
+    for (auto _ : state) {
+        GiNaC::ex s = eq.series(x==0, 6); // order 5 implies terms up to O(x^6)
+        benchmark::DoNotOptimize(s);
+    }
+}
+BENCHMARK(BM_GiNaC_DL);
+
+// ---------------------------------------------------------
+// 6. Benchmark: Intégrale Définie sur [0, 5]
+// ---------------------------------------------------------
+static void BM_SymAlgo_IntegraleDefinie(benchmark::State& state) {
+    ExprPtr expr = ast_pow(var("x"), 3.0) + (cst(2.0) * var("x"));
+    EquationClassique eq(expr);
+
+    for (auto _ : state) {
+        double val = eq.integraleDefinie(0.0, 5.0);
+        benchmark::DoNotOptimize(val);
+    }
+}
+BENCHMARK(BM_SymAlgo_IntegraleDefinie);
+
+static void BM_GiNaC_IntegraleDefinie(benchmark::State& state) {
+    GiNaC::symbol x("x");
+    GiNaC::ex eq = GiNaC::pow(x, 3) + 2 * x;
+
+    for (auto _ : state) {
+        GiNaC::ex int_eq = GiNaC::integral(x, 0, 5, eq);
+        GiNaC::ex val = int_eq.eval_integ();
+        benchmark::DoNotOptimize(val);
+    }
+}
+BENCHMARK(BM_GiNaC_IntegraleDefinie);
+
 BENCHMARK_MAIN();
